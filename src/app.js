26c07d999a91b5767c93aab1a50843df9682b406
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-09-10 21:16:47 +0100

    added patch endpoint to allow user to update lastFed property only

--- src/app.js
+++ src/app.js
@@ -7,26 +7,43 @@ app.use(express.json());
 
 
 app.get('/cats?', (request, response) => {
-    Cat.findAll( {where: request.query}).then(cats => ( response.status(200).json(cats)))
+    Cat.findAll( {where: request.query}).then(cats => ( response.status(200).json(cats)).catch(err => {
+        response.status(400).json({message: "error"})
+    }))
    
 })
 
 app.post('/cats', (request, response) => {
-    Cat.create(request.body).then(cat => response.status(201).json(cat))
+    Cat.create(request.body).then(cat => response.status(201).json(cat).catch(err=> {
+        response.status(400).json({message: "error"})
+    }))
     
 })
 
 app.get('/cats/:catId', (request, response) => {
-    Cat.findByPk(request.params.catId).then(cat => response.status(200).json(cat))
+    Cat.findByPk(request.params.catId).then(cat => response.status(200).json(cat)).catch(err => {
+        response.status(400).send({err})
+    })
 })
 
 app.patch('/cats/:catId', (request, response) => {
-    Cat.update(request.body, { where: { id: request.params.catId }}).then(cat => response.status(200).json(cat))
+    Cat.update(request.body, { where: { id: request.params.catId }}).then(cat => response.status(200).json(cat).catch(err => {
+        response.status(400).send({message: "error"})
+    }))
 })
 
 app.delete('/cats/:catId', (request, response) => {
-    Cat.destroy( {where: {id: request.params.catId}}).then(cat => response.status(200).json("deleted"))
+    Cat.destroy( {where: {id: request.params.catId}}).then(cat => response.status(200).json("deleted").catch(err => {
+        response.status(400).json({message: "error"})
+    }))
 })
+
+app.patch('/cats/feed/:catId', (request, response) => {
+    Cat.update({lastFed: new Date()}, {where: {id: request.params.catId}}).then(cat => response.status(200).json(cat).catch(err => {
+        response.status(400).json({message: "error"})
+    })) 
+})
+
 
 
 module.exports = app;
